414a4186bcce7c56ddcc134ecc1ed2fb29ffaf64
ecandre <elias-connor.andres@adp.com>
2023-01-27 15:53:59 +0100

    Qualifikation Detail Klasse und alles was dazu gehört. Wurde im letzen Commit ignoriert -.-

---
+++ src/app/qualification-detail/qualification-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qualification-detail',
+  templateUrl: './qualification-detail.component.html',
+  styleUrls: ['./qualification-detail.component.css']
+})
+export class QualificationDetailComponent {
+
+}
