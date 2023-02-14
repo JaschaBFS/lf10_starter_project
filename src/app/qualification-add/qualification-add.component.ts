3481cbe29099e4fd270fdb73bb87b5615a928c7d
JaschaBFS <jascha33@schule.bremen.de>
2023-02-14 19:58:11 +0100

    Qualification-add Komponente generiert

---
+++ src/app/qualification-add/qualification-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qualification-add',
+  templateUrl: './qualification-add.component.html',
+  styleUrls: ['./qualification-add.component.css']
+})
+export class QualificationAddComponent {
+
+}
